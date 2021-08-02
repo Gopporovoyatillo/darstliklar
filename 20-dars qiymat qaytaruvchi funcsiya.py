c193d93ed62bc3156da8351faa6bec61533ced3e
OLLIT.DEV <86409818+Gopporovoyatillo@users.noreply.github.com>
2021-08-02 19:35:59 +0500

    Update 20-dars qiymat qaytaruvchi funcsiya.py

--- 20-dars qiymat qaytaruvchi funcsiya.py
+++ 20-dars qiymat qaytaruvchi funcsiya.py
@@ -5,6 +5,7 @@ Created on Mon Aug  2 01:54:30 2021
 @author: Admin
 """
 
+
 def toliq_ism_yasa(ism, familiya):
     """Toliq isma qaytaruvchi funksiya"""
     toliq_ism = f"{ism} {familiya}"
@@ -27,6 +28,23 @@ talaba1 = toliq_ism_yasa('olim','hakimov') #otasining_ismi kiritilmadi
 talaba2 = toliq_ism_yasa('hakim','olimov','abrorovich')
 print(f"Darsga kelmagan talabalar: {talaba1} va {talaba2}")
 
+
+
+def oraliq(min,max):
+    sonlar = [] # bo'sh ro'yxat
+    while min<max:
+        sonlar.append(min)
+        min += 1
+    return sonlar
+
+print(oraliq(0,10))
+print(oraliq(10,21))
+
+print(oraliq(0,21,)) # 0 dan 21 gacha 2 qadam bilan
+
+
+
+
 def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
     avto = {'kompaniya':kompaniya,
             'model':model,
@@ -47,19 +65,7 @@ for avto in avtolar:
     else:
         narh = "Noma'lum"
     print(f"{avto['rang']} {avto['model']}. Narhi: {narh}")
-    
 
-def oraliq(min,max):
-    sonlar = [] # bo'sh ro'yxat
-    while min<max:
-        sonlar.append(min)
-        min += 1
-    return sonlar
-
-print(oraliq(0,10))
-print(oraliq(10,21))
-
-print(oraliq(0,21,2)) # 0 dan 21 gacha 2 qadam bilan
 
 print("Saytimizdagi avtolar ro'yxatini shakllantiramiz.")
 avtolar=[] # salondagi avtolar uchun bo'sh ro'yxat
@@ -69,14 +75,39 @@ while True:
     model=input("Modeli: ")
     rangi=input("Rangi: ")
     korobka=input("Korobka: ")
-    yili=input("Ishlab chiqarilgan yili: ")
-    narhi=input("Narhi: ")
+    yil=input("Ishlab chiqarilgan yili: ")
+    narh=input("Narhi: ")
     
     #Foydalanuvchi kiritdan ma'lumotlardan avto_info yordamida 
     #lug'at shakllantirib, har bir lug'atni ro'yxatga qo'shamiz:
-    avtolar.append(avto_info(kompaniya, model, rangi, korobka, yili, narhi))
+    avtolar.append(avto_info(kompaniya, model, rangi, korobka, yil, narh))
     
     # Yana avto qo'shish-qo'shmaslikni so'raymiz
     javob = input("Yana avto qo'shasizmi? (yes/no): ")
     if javob=='no':
         break
+print('Onlayn bozordagi mavjud avtomashinalar:')
+for avto in avtolar:
+    if avto['narh']:
+        narh = avto['narh']
+    else:
+        narh = "Noma'lum"
+    print(f"{avto['kompaniya']}  {avto['rang']} {avto['model']}  {avto['yil']} {avto['korobka']} {avto['narh']}")
+    
+    
+    
+#+ darsni tushinib  o'zimtomonimdan qilingan  q'oshimcha
+def avto_info(kompaniya, model, rangi, korobka, yili, narhi=None):
+    '''qiymat qaytaruvchi  6ta arumentiga ega funcsiya'''
+    avto = {'kompaniya':kompaniya,
+            'model':model,
+            'rang':rangi,
+            'korobka':korobka,
+            'yil':yili,
+            'narh':narhi}
+    print(f"{avto['kompaniya']}  {avto['rang']} {avto['model']}  {avto['yil']} {avto['korobka']} {avto['narh']}")
+  #  return avto
+
+
+    
+avto_info('gm','qizil','malibu','2000','avtomat','12000')
