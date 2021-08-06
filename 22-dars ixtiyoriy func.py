8bf9e2417e859a4b9cfb6dc9d2669e90bee50721
OLLIT.DEV <86409818+Gopporovoyatillo@users.noreply.github.com>
2021-08-06 21:32:11 +0500

    Add files via upload

---
+++ 22-dars ixtiyoriy func.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  6 01:00:02 2021
+
+@author: Admin
+"""
+
+def summa(*sonlar):
+    """Kiritilgan sonlar yig'indisini hisoblaydigan funksiya"""
+    yigindi = 0
+    for son in sonlar:  
+        yigindi += son
+    return yigindi
+
+print(summa(1,2))
+print(summa(1,2,3,4,5))
+print(summa(4,5,6,7))
+
+def summa(*sonlar):
+    """Kiritilgan sonlar yig'indisini hisoblaydigan funksiya"""
+    return sum(sonlar)
+
+print(summa(2))
+print(summa(1,2,3,4,5))
+print(summa(4,5,6,7))
+
+def summa(x,y,*sonlar):
+    """Kiritilgan sonlar yig'indisini hisoblaydigan funksiya"""
+    return x+y+sum(sonlar)
+
+print(summa(1,2))
+print(summa(1,2,3,4,5))
+print(summa(4,5,6,7))
+print(summa(2))
+
+
+def avto_info(kompaniya,model,**malumotlar):
+    """Avto haqidagi ma'lumotlarni lug'at ko'rinishdia qaytaruvchi funksiya"""
+    malumotlar['kompaniya']=kompaniya
+    malumotlar['model']=model
+    return malumotlar
+
+avto1 = avto_info("GM", "malibu", rang='qora', yil=2018)
+avto2 = avto_info("Kia", "K5", rang='qizil', narh=35000)
+
+print(avto2)
+print(avto1)
