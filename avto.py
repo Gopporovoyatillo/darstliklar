c0090bc7f91921e2ba4f42dff29e9d9436db7fe1
OLLIT.DEV <86409818+Gopporovoyatillo@users.noreply.github.com>
2021-08-23 01:51:19 +0500

    Add files via upload

--- avto.py
+++ avto.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 23 01:39:52 2021
+Created on Mon Aug 23 01:43:55 2021
 
 OLLIT.DEV
+
 
 @author: ACER
 """
 
-
-
-
 from uuid import uuid4
 class Avto:
     """Avtomobil klassi"""
+    __num_avto = 0
     def __init__(self,make,model,rang,yil,narh,km=0):
         """Avtomobilning xususiyatlari"""
         self.make = make
@@ -22,7 +21,12 @@ class Avto:
         self.narh = narh
         self.__km = km
         self.__id = uuid4()
-        
+        Avto.__num_avto += 1
+    
+    @classmethod
+    def get_num_avto(cls):
+        return cls.__num_avto
+    
     def get_km(self):
         return self.__km
     
@@ -36,7 +40,8 @@ class Avto:
         else:
             print("Mashina km kamaytirib bo'lmaydi")
 
-# avto1 = Avto("GM","Malibu","Qora",2020,40000,100000)
-# print(f"ID: {avto1.get_id()}")
-# avto1.add_km(1500)
-# print(avto1.get_km())
+class Bus:
+    pass
+
+class Train:
+    pass
